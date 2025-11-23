79c37d3b6f601af4f280a72adac5e9aa0cf37bbe
Faisal Osman <faisalosman@gmail.com>
2025-11-23 15:26:01 +0000

    Move slider component to new location

---
+++ components/ui/slider.tsx
@@ -0,0 +1,14 @@
+// Slider component
+
+import React from 'react';
+import './slider.css';
+
+const Slider = () => {
+    return (
+        <div className="slider">
+            {/* Slider implementation */}
+        </div>
+    );
+};
+
+export default Slider;
